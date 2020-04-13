3b8fb657e852ee72e839f05786801fb3cd1910fd
Marshall Abrams <marshall@logical.net>
2020-04-12 23:31:08 -0500

    initial commit of my initial experiments

--- src/app/lib.cljs
+++ src/app/lib.cljs
@@ -1,8 +1,7 @@
-
 (ns app.lib)
 
 (println "[lib] loading")
 
-(def c 1)
+(def c 147)
 
 (defonce d 2)
--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,14 +1,19 @@
-
 (ns app.main
   (:require [app.lib :as lib]))
 
-(def a 1)
+(def a 82)
 
 (defonce b 2)
 
+(def e "Yow, baby! whateva, uh huh.")
+
 (defn main! []
-  (println "[main]: loading"))
+  (let [x (.getElementById js/document "x")]
+    (set! (.-innerHTML x) "Jes startin'"))
+  (println "[main]: Yow!"))
 
 (defn reload! []
+  (let [x (.getElementById js/document "x")]
+    (set! (.-innerHTML x) e))
   (println "[main] reloaded lib:" lib/c lib/d)
   (println "[main] reloaded:" a b))
